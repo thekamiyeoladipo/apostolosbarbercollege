b3852e70c107ba176059ceb9a4c7ebf2ff5dd44c
kamiye <kamiye31@gmail.com>
2025-01-24 11:37:55 +0100

    graduands
    new section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
         serif: ['Merriweather', 'serif'],
       },
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'perspective(1000px) rotateY(0deg)'},
+          '100%': { transform: 'perspective(1000px) rotateY(360deg)'}
+        }
+      },
+      animation: {
+        rotate: 'rotate 30s linear infinite',
+      }
     },
   },
   plugins: [],
